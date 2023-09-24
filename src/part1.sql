74e36a25dfb1db5bc2a8418b0c7ca502a7b37b21
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-24 13:59:06 +0300

    creation data base of Customers and Cards

---
+++ src/part1.sql
@@ -0,0 +1,22 @@
+--  Cteate table Personal Data
+CREATE TABLE IF NOT EXISTS Personal_Data (
+    Customer_ID SERIAL PRIMARY KEY,
+    Customer_Name VARCHAR(25) NOT NULL,
+    Customer_Surname VARCHAR(50) NOT NULL,
+    Customer_Primary_Email VARCHAR(50) NOT NULL,
+    Customer_Primary_Phone VARCHAR(50) NOT NULL,
+
+    CONSTRAINT persinfo_customer_id_pkey PRIMARY KEY (customer_id),
+    CONSTRAINT persinfo_name_check CHECK (customer_name ~ '^[A-ZА-Я][a-zа-яё -]+$'),
+    CONSTRAINT persinfo_surname_check CHECK (customer_surname ~ '^[A-ZА-Я][a-zа-яё -]+$'),
+    CONSTRAINT persinfo_email_check CHECK (customer_primary_email ~ '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+    CONSTRAINT persinfo_phone_check CHECK (customer_primary_phone ~ '^[+][7][0-9]{10}')
+);
+
+CREATE TABLE IF NOT EXISTS Cards (
+    Customer_Card_ID SERIAL PRIMARY KEY,
+    Customer_ID INT NOT NULL,
+
+    CONSTRAINT cards_customer_card_id_pkey PRIMARY KEY (Customer_Card_ID),
+    CONSTRAINT customer_id_fkey FOREIGN KEY (Customer_ID) references Personal_Data(Customer_ID)
+);
