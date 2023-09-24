7251ee0b0e1e94ab87ccfce5c0e055de7b0f0eb0
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-24 15:57:42 +0300

    creation checks and transactions tables

--- src/part1.sql
+++ src/part1.sql
@@ -20,3 +20,38 @@ CREATE TABLE IF NOT EXISTS Cards (
     CONSTRAINT cards_customer_card_id_pkey PRIMARY KEY (Customer_Card_ID),
     CONSTRAINT customer_id_fkey FOREIGN KEY (Customer_ID) references Personal_Data(Customer_ID)
 );
+
+CREATE TABLE IF NOT EXISTS Transactions (
+    Transaction_ID SERIAL PRIMARY KEY,
+    Customer_Card_ID INT NOT NULL,
+    Transaction_Summ NUMERIC NOT NULL,
+    Transaction_DateTime TIMESTAMP NOT NULL,
+    Transaction_Store_ID INT NOT NULL,
+
+    CONSTRAINT transactions_transaction_id_pkey PRIMARY KEY (Transaction_ID),
+    CONSTRAINT transactions_customer_card_id_fkey FOREIGN KEY Cards(Customer_Card_ID),
+    CONSTRAINT transactions_summ_check CHECK (Transaction_Summ >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS Checks (
+    Transaction_ID SERIAL PRIMARY KEY,
+    SKU_ID SERIAL PRIMARY KEY,
+    SKU_Amount INT NOT NULL,
+    SKU_Summ NUMERIC NOT NULL,
+    SKU_Summ_Paid NUMERIC NOT NULL,
+    SKU_Discount NUMERIC NOT NULL,
+
+    CONSTRAINT checks_sku_id_pkey PRIMARY KEY (SKU_ID),
+    CONSTRAINT checks_sku_summ_paid_id_fkey FOREIGN KEY Transactions(Transaction_Summ)
+
+);
+
+CREATE TABLE IF NOT EXISTS Product_Grid (
+    SKU_ID SERIAL PRIMARY KEY,
+    SKU_Name VARCHAR(50) NOT NULL,
+    Group_ID INT NOT NULL,
+
+    CONSTRAINT produduct_grid_sku_id_pkey PRIMARY KEY (SKU_ID),
+    
+
+);
